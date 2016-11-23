6a4f819377abf42f6c3a43389e6017652175315a
Daan Mortier <tjoekbezoer@gmail.com>
2016-11-23 17:44:10 +0100

    Idea: Started using roleplay as auth system

--- src/routes/idea.js
+++ src/routes/idea.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var db      = require('../db');
+var auth    = require('../auth');
 var errors  = require('../errors');
 
 module.exports = function( app ) {
-	var router = express.Router();
-	app.use('/ideas', router);
-	
-	router.get('/', function getCurrentIdeas( req, res ) {
+	// Idea index page
+	// ---------------
+	app.get('/ideas', auth.can('ideas:view'), function( req, res ) {
 		res.send('List ideas');
 	});
 	
-	router.get('/:ideaId', fetchIdea, function viewIdeaDetails( req, res ) {
+	// Single idea
+	// -----------
+	var router = express.Router();
+	app.use('/idea', router);
+	
+	router.all('/:ideaId', fetchIdea);
+	router.get('/:ideaId', auth.can('idea:view'), function( req, res ) {
 		var idea = req.resource;
 		res.format({
 			html: function() {
