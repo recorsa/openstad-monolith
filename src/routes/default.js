ab8ffeaacc2c82b928cd81a55aef010441dc6432
Daan Mortier <tjoekbezoer@gmail.com>
2016-11-23 21:49:49 +0100

    Added index page based on base.njk

--- src/routes/default.js
+++ src/routes/default.js
@@ -2,7 +2,6 @@ var db = require('../db');
 
 module.exports = function( app ) {
 	app.get('/', function welcome( req, res ) {
-		var message = req.session.userId ? 'Logged in' : 'Not logged in';
-		res.send('<h1>Welcome</h1>'+message);
+		res.render('index.njk');
 	});
 }
