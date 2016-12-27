3d30c263e6a891b4760712c9971b7b97be8bb2f4
Daan Mortier <tjoekbezoer@gmail.com>
2016-12-27 14:26:58 +0100

    Server: Simplified starting, improved stack trace on error
    `db.sync` was useless and complicated the stack trace.

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ process.env.DEBUG = config.get('logging');
 // -----------------
 var Server  = require('./src/Server');
 var Cron    = require('./src/Cron');
-var db      = require('./src/db');
 
-db.sequelize.sync().then(function() {
-	Cron.start();
-	Server.start(config.get('express.port'));
-}).catch(function( e ) {
-	db.sequelize.close();
-	throw e;
-});
+Cron.start();
+Server.start(config.get('express.port'));
