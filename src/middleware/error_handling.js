ad81e3bc09879a8bab53a9ece6f99f8c9d1e0eb5
Daan Mortier <tjoekbezoer@gmail.com>
2017-01-17 18:23:29 +0100

    Started working on general error handler

--- src/middleware/error_handling.js
+++ src/middleware/error_handling.js
@@ -1,10 +1,27 @@
+var statuses = require('statuses');
+
 module.exports = function( app ) {
-	// Authorisation errors.
-	app.use(function handleAppError( err, req, res, next ) {
-		if( err.status ) {
-			res.status(err.status).send(err.message || 'Unknown error');
-		} else {
-			next(err);
+	app.use(function handleError( err, req, res, next ) {
+		var env            = app.get('env');
+		var status         = err.status || 500;
+		var friendlyStatus = statuses[status]
+		var stack          = err.stack || err.toString();
+		var message        = err.message;
+		// Production get  s a default message.
+		var errorMessage   = env === 'development' || req.user.isAdmin() ?
+		                     stack :
+		                     '';
+		
+		if( env !== 'test' ) {
+			console.error(stack);
 		}
+		
+		res.status(status);
+		res.out('error', true, {
+			status         : status,
+			friendlyStatus : friendlyStatus,
+			errorMessage   : errorMessage.replace(/\x20{2}/g, ' &nbsp;'),
+			message        : message
+		})
 	});
 };
