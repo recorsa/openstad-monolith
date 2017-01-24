782ebff36020786bb3a94141203d5b2ead373e3e
Daan Mortier <tjoekbezoer@gmail.com>
2017-01-24 16:09:09 +0100

    Articles: Added data model and fixtures

---
+++ fixtures/data/articles.js
@@ -0,0 +1,95 @@
+var moment    = require('moment-timezone');
+var startDate = moment().subtract(24, 'days').startOf('day');
+
+module.exports = [{
+	id          : 1,
+	createdAt   : moment(startDate).add(24, 'days'),
+	image       : '',
+	title       : 'Jeroen wil dynamiek!',
+	summary     : `Jeroen wil meer dynamiek in West! Hij wil bewoners enthousiasmeren mee te denken over de kansen die er liggen om hun buurt nóg beter en leuker te maken.`,
+	intro       : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+	quote       : `Ut enim ad minim veniam, quis nostrud exercitation`,
+	description : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+}, {
+	id          : 2,
+	createdAt   : moment(startDate).add(19, 'days'),
+	image       : '',
+	title       : 'Hugo de Grootkade autovrij',
+	summary     : `Bewoner Mischa Woutersen had een bijzonder voorstel: straten in West een maand lang autovrij maken. Zijn plan werd werkelijkheid! In de zomer werd de eerste autovrije straat gerealiseerd: de Hugo de Grootkade! `,
+	intro       : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+	quote       : `Ut enim ad minim veniam, quis nostrud exercitation`,
+	description : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+}, {
+	id          : 3,
+	createdAt   : moment(startDate).add(3, 'days'),
+	image       : '',
+	title       : 'Bernadette wil meer doen met ideeën uit de buurt',
+	summary     : `Huiswerklessen geven, koken voor anderen of op elkaars kinderen passen: in haar wijk gebeurt het allemaal. Ontmoet Bernadette Vieverich.`,
+	intro       : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+	quote       : `Ut enim ad minim veniam, quis nostrud exercitation`,
+	description : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+}, {
+	id          : 4,
+	createdAt   : moment(startDate).add(14, 'days'),
+	image       : '',
+	title       : 'Samen voor je buurt zorgen!',
+	summary     : `Jelle vindt dat burgers zelf verantwoordelijkheid moeten krijgen. Hij leerde dit bij het jongerencentrum waar hij actief is. Iedereen heeft de sleutel van het gebouw, waardoor bewoners zich samen verantwoordelijk voelen.`,
+	intro       : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+	quote       : `Ut enim ad minim veniam, quis nostrud exercitation`,
+	description : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+}, {
+	id          : 5,
+	createdAt   : moment(startDate).add(10, 'days'),
+	image       : '',
+	title       : `‘West heeft een ruig randje en is lekker mixed.’`,
+	summary     : `Alexander IJkelenstam leeft in een buurt die bij hem past.`,
+	intro       : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+	quote       : `Ut enim ad minim veniam, quis nostrud exercitation`,
+	description : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+}, {
+	id          : 6,
+	createdAt   : moment(startDate).add(7, 'days'),
+	image       : '',
+	title       : `‘Samenwerken is belangrijk’`,
+	summary     : `De Stem van West is een mooi voorbeeld van co-creatie en samenwerking tussen inwoners en het Algemeen Bestuur, vindt Bjørn Lanser.`,
+	intro       : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+	quote       : `Ut enim ad minim veniam, quis nostrud exercitation`,
+	description : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+}, {
+	id          : 7,
+	createdAt   : moment(startDate).add(5, 'days'),
+	image       : '',
+	title       : 'Bewoners kiezen zelf architect',
+	summary     : 'Bij de herinrichting van het Karel Doormanplein besloot een groep actieve bewoners om ook om het jongerencentrum ‘New Society’ op te knappen. Hiertoe hebben zij zelf een architect aan het stadsdeel voorgedragen.',
+	intro       : 'Bij de herinrichting van het Karel Doormanplein vond een groep actieve bewoners dat niet alleen rekening gehouden moest worden met de fysieke indeling van het plein maar ook dat het jongerencentrum ‘New Society’ opgeknapt moest worden. Doordat buurtbewoners Lex de Jong en Harry Goossen precies weten hoe je een plan kunt aandragen bij het Stadsdeel, kregen ze het voor elkaar dat de gemeente niet alleen betaalde voor de opknapbeurt, maar dat de buurt zelf een architect aandroeg. Een sterk staaltje van samenwerking!',
+	quote       : 'Wat niet op de agenda staat, wordt niet besproken. Het is dus zaak te weten hoe je iets op de agenda krijgt.',
+	description : `
+		<p>Lex de Jong weet inmiddels hoe je een goed plan ook echt uitgevoerd krijgt. “Eerlijk gezegd, wist ik van tevoren niet waar ik aan begonnen was. Maar dankzij mijn buurman Harry Goossen leerde ik langzaamaan de weg naar het Stadsdeel kennen en konden we samen de juiste stappen zetten om de gemeente ervan te overtuigen dat wij heel goed wisten wat onze buurt nodig had.”</p>
+		<p>In 2011 maakte het Stadsdeel plannen voor de renovatie van het Karel Doormanplein. Plannen die volgens de bewoners veels te gericht waren op ‘stenen’ ofwel het hernieuwd betegelen. Dat was het moment dat een klein groepje actieve buurtbewoners samen ging zitten om te brainstormen over de wijze waarop de gemeente misschien op andere gedachten gebracht kon worden. Ze besloten het op de agenda van de deelraad te zetten.</p>
+		<p>“Die ‘agenda’ is heel belangrijk” benadrukt Lex. “Wat niet op de agenda staat, wordt ook niet besproken. Het is dus zaak te weten hoe je iets op die agenda krijgt. Gelukkig wist Harry mij te vertellen dat iedere bewoner zich kan aanmelden om drie minuten spreektijd te krijgen bij een deelraadvergadering. Je moet dan wel een goed voorbereid plan hebben.”</p>
+		<p>“Uiteindelijk hebben we de raadsleden een boekje aangeboden met daarin voorbeelden van andere pleinen en met ideeën voor onze buurt, zodat we een goed onderbouwd verhaal hadden. Het letterlijke verhaal mocht ik in de raadszaal doen. Dat was wel een mijlpaal voor me, want je spreekt toch namens je buurt!”</p>
+		<p>Het Stadsdeel reageerde heel voortvarend en in korte tijd werden er concrete plannen gemaakt voor het plein en werden er twee architecten benaderd om ontwerpen in te dienen. “Ja, dat klinkt heel leuk” zegt Lex, “maar die mensen waren nog nooit op het plein geweest en hadden geen enkele bewoner gesproken. Dus je kunt je voorstellen dat de plannen niet aansloten bij onze wensen als buurtbewoners.”</p>
+		<p>Het gevolg was dat Lex, Harry en buurtbewoner Ingeborg Baltussen aparte bewonersavonden organiseerden in hun favoriete ontmoetingsplek, het typisch Amsterdams café ‘Van de Buurt’, zelf van deur tot deur gingen om handtekeningen op te halen en later zelfs een wijkkrantje gingen maken om bewoners te enthousiasmeren. “We deden alles zelf. Sterker nog, we vonden een architect bereid om een ontwerp te maken en binnen anderhalve week hadden we een projectplan dat we vervolgens presenteerden aan het dagelijks bestuur.”</p>
+		<p>Als gevolg van de actieve dialoog met de gemeente, kregen Lex, Harry en Ingeborg gelegenheid om de architect aan te dragen en werd het ontwerp van deze architect geselecteerd en uitgevoerd. Het akkoord om verder te gaan met de architect van hun keuze werd naast de 1,3 miljoen euro voor de herinrichting van het plein nog eens verhoogd met vierhonderd duizend euro extra voor het verbouwen en opknappen van het jongerencentrum.</p>
+		<p>Lex is trots op het resultaat: “Ik zie het als een soort spel. Als je weet hoe de regels in elkaar steken, dan kun je heel veel bereiken. Zowel de gemeente als de bewoners hebben er baat bij dat er nauw samengewerkt wordt. Je moet met elkaar in contact blijven. En ik weet nu in ieder geval hoe!”</p>
+		<p>Heb je ook een goed idee? <a href="/idea/new">Upload dan hier je plan</a>.</p>
+	`
+}, {
+	id          : 8,
+	createdAt   : moment(startDate).add(1, 'days'),
+	image       : '',
+	title       : 'Klagen of afspraken maken?',
+	summary     : `Je kunt klagen over afval, maar je kunt ook samen afspraken maken. Dat is een veel prettigere manier om met elkaar om te gaan in de wijk, vindt Onika.`,
+	intro       : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+	quote       : `Ut enim ad minim veniam, quis nostrud exercitation`,
+	description : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+}, {
+	id          : 9,
+	createdAt   : moment(startDate),
+	image       : '',
+	title       : 'Met kleine stapjes tot grote veranderingen komen',
+	summary     : `Een veranderproces is zwaar, maar met kleine stapjes kun je steeds verder komen. Ralph Stuyver weet inmiddels hoe hij zijn buurt stapje voor stapje mooier kan maken.`,
+	intro       : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+	quote       : `Ut enim ad minim veniam, quis nostrud exercitation`,
+	description : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+}];
