943d377f9df3b3fd0ceff0b792213e564bb00f11
Daan Mortier <tjoekbezoer@gmail.com>
2017-01-24 16:10:06 +0100

    Articles: Added article page

---
+++ src/routes/article.js
@@ -0,0 +1,32 @@
+var config      = require('config')
+  , createError = require('http-errors')
+  , express     = require('express')
+  , Promise     = require('bluebird');
+var db          = require('../db');
+
+module.exports = function( app ) {
+	var router = express.Router();
+	app.use('/article', router);
+	
+	router.route('/:articleId')
+	.all(fetchArticle)
+	.get(function( req, res, next ) {
+		res.out('article', true, {
+			article: req.article
+		});
+	});
+};
+
+function fetchArticle( req, res, next ) {
+	var articleId = req.params.articleId;
+	db.Article.findById(articleId)
+	.then(function( article ) {
+		if( !article ) {
+			throw createError(404, 'Artikel niet gevonden');
+		} else {
+			req.article = article;
+			next();
+		}
+	})
+	.catch(next);
+}
