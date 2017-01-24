1dd2a8ab07b09e8341a0b24c099c92800785ebe7
Daan Mortier <tjoekbezoer@gmail.com>
2017-01-24 16:03:49 +0100

    Fixtures: Moved meetings to separate file
    So they can be reused for a future ‘production fixtures’ script.

--- fixtures/dev.js
+++ fixtures/dev.js
@@ -3,11 +3,13 @@ var _      = require('lodash')
   , moment = require('moment-timezone')
 var log    = require('debug')('app:db');
 
+var meetings = require('./data/meetings');
 module.exports = co.wrap(function*( db ) {
 	log('generating meetings...');
 	yield meetings.map(function( meetingData ) {
 		return db.Meeting.create(meetingData);
 	});
+	
 	log('generating users and ideas...');
 	yield users.map(function( userData ) {
 		return db.User.create(userData, {
@@ -25,40 +27,15 @@ module.exports = co.wrap(function*( db ) {
 			}]
 		});
 	});
+	
 	log('test database complete');
 });
 
-var today = moment().endOf('day');
-
-// Meetings
-// --------
-var meetings = [
-	{id: 1,  date: moment(today).day(5).subtract(8, 'weeks').toDate()},
-	{id: 2,  date: moment(today).day(5).subtract(6, 'weeks').toDate()},
-	{id: 3,  date: moment(today).day(5).subtract(4, 'weeks').toDate()},
-	{id: 4,  date: moment(today).day(5).subtract(2, 'weeks').toDate()},
-	{id: 5,  date: moment(today).day(5).toDate()},
-	{id: 6,  date: moment(today).day(5).add(2,  'weeks').toDate()},
-	{id: 7,  date: moment(today).day(5).add(4,  'weeks').toDate()},
-	{id: 8,  date: moment(today).day(5).add(6,  'weeks').toDate()},
-	{id: 9,  date: moment(today).day(5).add(8,  'weeks').toDate()},
-	{id: 10, date: moment(today).day(5).add(10, 'weeks').toDate()},
-	{id: 11, date: moment(today).day(5).add(12, 'weeks').toDate()},
-	{id: 12, date: moment(today).day(5).add(14, 'weeks').toDate()},
-	{id: 13, date: moment(today).day(5).add(16, 'weeks').toDate()},
-	{id: 14, date: moment(today).day(5).add(18, 'weeks').toDate()},
-	{id: 15, date: moment(today).day(5).add(20, 'weeks').toDate()},
-	{id: 16, date: moment(today).day(5).add(22, 'weeks').toDate()},
-	{id: 17, date: moment(today).day(5).add(24, 'weeks').toDate()},
-	{id: 18, date: moment(today).day(5).add(26, 'weeks').toDate()},
-	{id: 19, date: moment(today).day(5).add(28, 'weeks').toDate()},
-	{id: 20, date: moment(today).day(5).add(30, 'weeks').toDate()},
-	{id: 21, date: moment(today).day(5).add(32, 'weeks').toDate()},
-	{id: 22, date: moment(today).day(5).add(34, 'weeks').toDate()},
-	{id: 23, date: moment(today).day(5).add(36, 'weeks').toDate()}
-];
 // Users including their ideas
 // ---------------------------
+// This data is only used for the dev fixtures, so no shared file in
+// `data` folder required.
+var today = moment().endOf('day');
 var users = [
 	{id : 1 , complete : 0 , role : 'unknown'},
 	{id : 2 , complete : 1 , role : 'admin' ,      email: 'tjoekbezoer@gmail.com'         , password : 'password'        , firstName : 'Bastard Operator' , lastName : 'from Hell' , gender : 'male' , ideas : [
