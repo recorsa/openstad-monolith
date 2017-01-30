e307fb08a236b704e1191061c3728a07ffca8c46
Daan Mortier <tjoekbezoer@gmail.com>
2017-01-30 23:15:40 +0100

    Mail: Refactoring, added SendGrid support

--- src/mail.js
+++ src/mail.js
@@ -6,18 +6,32 @@ var debug      = require('debug');
 var log        = debug('app:mail:sent');
 var logError   = debug('app:mail:error');
 
-var transporter    = nodemailer.createTransport(config.get('mail.transport'));
-var defaultOptions = {
+var method     = config.get('mail.method');
+var transport  = config.get('mail.transport');
+
+var transporter;
+switch( method ) {
+	case 'smtp':
+		transporter = nodemailer.createTransport(transport.smtp);
+		break;
+	case 'sendgrid':
+		var sendGrid = require('nodemailer-sendgrid-transport');
+		var sgConfig = sendGrid(transport.sendgrid);
+		transporter  = nodemailer.createTransport(sgConfig);
+		break;
+}
+
+// Default options for a single email.
+var defaultSendMailOptions = {
 	from    : config.get('mail.from'),
 	subject : 'No title',
 	text    : 'No message'
 };
-
 module.exports = {
 	sendMail: function( options ) {
 		return new Promise(function( resolve, reject ) {
 			transporter.sendMail(
-				defaults(options, defaultOptions),
+				defaults(options, defaultSendMailOptions),
 				function( error, info ) {
 					if( error ) {
 						logError(error)
