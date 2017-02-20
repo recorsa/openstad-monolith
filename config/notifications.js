fdc5bd6262283ed2c975a736426e055c6483111a
Daan Mortier <tjoekbezoer@gmail.com>
2017-02-20 18:23:06 +0100

    Notifications: Introduced ‘admin’ publication
    This is the first step towards fully working notifications: a high frequency notifications email for the site admins.
    
    Only takes idea/arg creation/updating into account.

--- config/notifications.js
+++ config/notifications.js
@@ -1,14 +1,5 @@
-var log           = require('debug')('app:notifications');
-var db            = require('../src/db');
 var notifications = require('../src/notifications');
 
-db.User.findAll({where: {role: 'admin'}})
-.then(function( users ) {
-	return users.map(function( user ) {
-		return notifications.subscribe('email', user.id, null, null, ['*']);
-	});
-})
-.all()
-.then(function() {
-	log('subscribed admins to all email events');
-});
+// Subscribe non-existent user. Will be used to send mail to the site
+// administrator's email address.
+notifications.subscribe('admin', 0, null, null, ['*']);
