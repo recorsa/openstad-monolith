16a4d2023b88f6dddc95bf9f954b51dfaaca74de
Daan Mortier <tjoekbezoer@gmail.com>
2017-02-22 16:45:49 +0100

    Agenda: Improved design
    - TEMPORARY STATIC VIEW.
    - Added selection/meeting dates. Code/DB needs to be changed to incorporate this change in flow.

--- src/models/Meeting.js
+++ src/models/Meeting.js
@@ -1,3 +1,25 @@
+// 14 februari 2017 - peildatum
+// 7 maart 2017 - vergaderdatum
+// 21 maart 2017 - peildatum
+// 4 april 2017 - vergaderdatum
+// 18 april 2017 - peildatum
+// 9 mei 2017 - vergaderdatum
+// 23 mei, 2017 - peildatum
+// 6 juni 2017 - vergaderdatum
+// 20 juni 2017 - peildatum
+// 4 juli 2017 - vergaderdatum & peildatum ?
+// 18 juli 2017 - vergaderdatum
+// 
+// begin september een peildatum afspreken?
+// 
+// 12 september 2017 - vergaderdatum
+// 26 september 2017 - peildatum
+// 10 oktober 2017 - vergaderdatum 
+// 31 oktober 2017 - peildatum
+// 14 november 2017 - vergaderdatum
+// 28 november 2017 - peildatum
+// 12 december 2017 - vergaderdatum
+
 module.exports = function( db, sequelize, DataTypes ) {
 	var Meeting = sequelize.define('meeting', {
 		date: DataTypes.DATE
