23704ee60918e189d3a4f4995fe05cd988519a48
Daan Mortier <tjoekbezoer@gmail.com>
2017-03-27 13:56:12 +0200

    Ideas: Display duration in days instead of months

--- config/moment.js
+++ config/moment.js
@@ -5,3 +5,7 @@ moment.tz.setDefault(config.get('timeZone'));
 // TODO: Set this per request based on accept-language header or
 //       logged in user preference?
 moment.locale(config.get('locale'));
+// The custom nunjuck filter `duration` uses moment's `humanize` to
+// display the remaining time. This threshold forces `humanize` to
+// calculate the time in days instead of months.
+moment.relativeTimeThreshold('d', config.get('ideas.duration')+1);
