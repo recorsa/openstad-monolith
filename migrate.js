2ecaf5274ae885a043728da5d28c576a00d70b70
Daan Mortier <tjoekbezoer@gmail.com>
2017-07-12 16:26:21 +0200

    Migrations: Support for new installations
    When the script detects that no migrations have yet been executed, it assumes this is a new installation, and logs all existing migrations (since they are assumed to have been implemented into the models).

--- migrate.js
+++ migrate.js
@@ -1,5 +1,6 @@
-var Umzug = require('umzug');
-var db    = require('./src/db');
+var Promise = require('bluebird');
+var Umzug   = require('umzug');
+var db      = require('./src/db');
 
 var umzug = new Umzug({
 	storage: 'sequelize',
@@ -14,11 +15,39 @@ var umzug = new Umzug({
 	}
 });
 
-umzug.up()
+umzug.executed()
 .then(function( migrations ) {
-	var fileNames = migrations.map(m => m.file);
-	console.log(fileNames.join('\n'));
+	return !migrations.length ?
+	       logAllMigrations() :
+	       performMigrations();
+})
+.catch(function( e ) {
+	console.log(e);
 })
 .finally(function() {
 	process.exit();
 });
+
+function logAllMigrations() {
+	console.log('New installation, marking all migrations as done...');
+	return umzug.pending()
+	.then(function( migrations ) {
+		return Promise.all(migrations.map(logMigration));
+	});
+}
+function logMigration( migration ) {
+	console.log(migration.file);
+	return umzug.storage.logMigration(migration.file);
+}
+function performMigrations() {
+	return umzug.up()
+	.then(function( migrations ) {
+		if( !migrations.length ) {
+			console.log('No new migrations');
+		} else {
+			console.log('Executed migrations:');
+			var fileNames = migrations.map(m => m.file);
+			console.log(fileNames.join('\n'));
+		}
+	});
+}
