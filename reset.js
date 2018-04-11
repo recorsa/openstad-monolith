ac99613c0418e444529fc9b91a57c690a4f69ca7
Daan Mortier <tjoekbezoer@gmail.com>
2018-04-11 21:43:10 +0200

    DevOps: Refactored `reset.js`
    Since `db.js` requires a working authorization configuration, `NODE_ENV` needs to be set — because of recent changes in the `config` structure.

--- reset.js
+++ reset.js
@@ -4,20 +4,14 @@ var Promise = require('bluebird');
 Promise.longStackTraces();
 process.env.DEBUG = config.get('logging');
 
-var db = require('./src/db');
-
-if( !config.get('debug') ) {
-	return console.error('Not in debug mode');
-}
-
-var appName = process.argv[2];
+var appName = process.env.NODE_ENV;
 if( !appName ) {
-	return console.error(`Run as \`node reset NODE_ENV\`
+	return console.error(`Run as \`NODE_ENV=appName node reset\`
 
-where NODE_ENV is set to the value used to run
-the application (\`NODE_ENV=appName node server\`)`);
+where NODE_ENV is set to the value used to run the server`);
 }
 
+var db = require('./src/db');
 db.sequelize.sync({force: true}).then(function() {
 	return require(`./fixtures/${appName}`)(db);
 })
