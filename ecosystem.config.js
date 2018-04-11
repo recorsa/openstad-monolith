c12004d5833a08e8bdc225bdef890a810b6e2774
Daan Mortier <tjoekbezoer@gmail.com>
2018-04-11 17:35:30 +0200

    DevOps: Updated ecosystem to reflect `config` changes
    - `NODE_ENV` and `NODE_APP_INSTANCE` are now essential for a successful application start.
    - Added `app.instance_var` to avoid PM2 possibly mutating `NODE_APP_INSTANCE` env variable for (internal) fork/cluster functionality.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,90 +1,82 @@
 var config = {
-	apps : [{
-		name      : 'stem-staging',
-		script    : 'server.js',
-		node_args : '--use-strict',
-		env: {
-			NODE_ENV                   : 'stemvan',
-			NODE_APP_INSTANCE          : 'staging',
-			BLUEBIRD_LONG_STACK_TRACES : 1,
-			BLUEBIRD_WARNINGS          : 0
-		}
-	}],
-	
-	deploy : {
-		staging : {
-			user          : 'daan',
-			host          : '185.110.174.172',
-			path          : '/var/www/destemvanwest.openstadsdeel.nl/www',
-			
-			ref           : 'origin/staging',
-			repo          : 'ssh://git@git.daanmortier.nl/abtool',
-			
-			env           : {
-				NODE_ENV : 'production'
-			},
-			'post-deploy' : 'git submodule init && git submodule update && npm install && node migrate.js && pm2 startOrRestart ecosystem.config.js --only stem-staging --update-env'
-		}
-	}
+	apps : [],
+	deploy : {}
 };
 // Production deployments
 // ----------------------
-addProductionApp('stemvan', [{
+addApp('stemvan', [
+// `stemvan` staging
+// -----------------
+{
+	appName    : 'stem-staging',
+	deployName : 'staging',
+	remotePath : '/var/www/stemvanwest.openstadsdeel.nl/www',
+	ref        : 'origin/staging'
+}, 
+// `stemvan` production
+// --------------------
+{
 	appName    : 'stem-prod',
 	deployName : 'production',
-	remotePath : '/var/www/stemvanwest.amsterdam.nl/www'
+	remotePath : '/var/www/stemvanwest.amsterdam.nl/www',
+	ref        : 'origin/production'
 }, {
 	appName    : 'stem-centrum',
 	deployName : 'production_centrum',
-	remotePath : '/var/www/stemvancentrum.amsterdam.nl/www'
+	remotePath : '/var/www/stemvancentrum.amsterdam.nl/www',
+	ref        : 'origin/production'
 }, {
 	appName    : 'stem-oost',
 	deployName : 'production_oost',
-	remotePath : '/var/www/stemvanoost.amsterdam.nl/www'
+	remotePath : '/var/www/stemvanoost.amsterdam.nl/www',
+	ref        : 'origin/production'
 }, {
 	appName    : 'stem-nieuwwest',
 	deployName : 'production_nieuwwest',
-	remotePath : '/var/www/stemvannieuwwest.amsterdam.nl/www'
+	remotePath : '/var/www/stemvannieuwwest.amsterdam.nl/www',
+	ref        : 'origin/production'
 }, {
 	appName    : 'stem-zuidoost',
 	deployName : 'production_zuidoost',
-	remotePath : '/var/www/stemvanzuidoost.amsterdam.nl/www'
+	remotePath : '/var/www/stemvanzuidoost.amsterdam.nl/www',
+	ref        : 'origin/production'
 }]);
 module.exports = config;
 
 // Helper
 // ------
 // Automates configuring production deployments to keep it DRY.
-function addProductionApp( env, app ) {
+function addApp( env, app ) {
 	if( app instanceof Array ) {
-		return app.forEach(addProductionApp.bind(null, env));
+		return app.forEach(addApp.bind(null, env));
 	}
 	if( app.constructor !== Object ) {
 		return;
 	}
 	
+	var env = {
+		NODE_ENV                   : env,
+		NODE_APP_INSTANCE          : 'production',
+		BLUEBIRD_LONG_STACK_TRACES : 1,
+		BLUEBIRD_WARNINGS          : 0
+	};
+	
 	config.apps.push({
-		name      : app.appName,
-		script    : 'server.js',
-		node_args : '--use-strict',
-		env : {
-			NODE_ENV                   : env,
-			NODE_APP_INSTANCE          : 'production',
-			BLUEBIRD_LONG_STACK_TRACES : 1,
-			BLUEBIRD_WARNINGS          : 0
-		}
+		name         : app.appName,
+		script       : 'server.js',
+		node_args    : '--use-strict',
+		instance_var : 'INSTANCE_ID',
+		env          : env
 	});
 	config.deploy[app.deployName] = {
 		user          : 'daan',
 		host          : '185.110.174.172',
 		path          : app.remotePath,
 		
-		ref           : 'origin/production',
+		ref           : app.ref,
 		repo          : 'ssh://git@git.daanmortier.nl/abtool',
 		
-		env           : {
-			NODE_ENV : 'production'
-		},
-		'post-deploy' : `git submodule init && git submodule update && npm install && node migrate.js && pm2 startOrRestart ecosystem.config.js --only ${app.appName}`
+		env           : env,
+		'post-deploy' : `git submodule init && git submodule update && npm install && node migrate.js && pm2 startOrRestart ecosystem.config.js --only ${app.appName} --update-env`
 	};
 }
