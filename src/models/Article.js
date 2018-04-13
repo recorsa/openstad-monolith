92aa85440dddcfef23882b9a486ca11af50aacdb
Niels Vegter <Niels Vegter>
2018-04-13 13:13:36 +0000

    Add image to article model

--- src/models/Article.js
+++ src/models/Article.js
@@ -25,6 +25,20 @@ module.exports = function( db, sequelize, DataTypes ) {
 				this.setDataValue('title', sanitize.noTags(text));
 			}
 		},
+		posterImageUrl: {
+			type         : DataTypes.VIRTUAL,
+			get          : function() {
+				var posterImage = this.get('posterImage');
+				var location    = this.get('location');
+				
+				return posterImage ? `/image/${posterImage.key}?thumb` :
+				       location    ? 'https://maps.googleapis.com/maps/api/streetview?'+
+				                     'size=800x600&'+
+				                     `location=${location.coordinates[0]},${location.coordinates[1]}&`+
+				                     'heading=151.78&pitch=-0.76&key=AIzaSyCrp_kqFQoKEaW5DOEBVjAu61cRl3-T0Lg'
+				                   : null;
+			}
+		},
 		summary: {
 			type         : DataTypes.TEXT,
 			allowNull    : false,
@@ -58,12 +72,46 @@ module.exports = function( db, sequelize, DataTypes ) {
 			scopes: scopes,
 			associate: function( models ) {
 				this.belongsTo(models.User);
+				this.hasMany(models.Image);
+				this.hasOne(models.Image, {as: 'posterImage'});
 			},
 			
 			getTiles: function() {
 				return this.scope('asTile').findAll();
-			}
-		}
+			},
+
+			updateImages: function( imageKeys ) {
+				var self = this;
+				if( !imageKeys || !imageKeys.length ) {
+					imageKeys = [''];
+				}
+				
+				var ideaId  = this.id;
+				var queries = [
+					db.Image.destroy({
+						where: {
+							ideaId : ideaId,
+							key    : {$notIn: imageKeys}
+						}
+					})
+				].concat(
+					imageKeys.map(function( imageKey, sort ) {
+						return db.Image.update({
+							ideaId : ideaId,
+							sort   : sort
+						}, {
+							where: {key: imageKey}
+						});
+					})
+				);
+				
+				return Promise.all(queries).then(function() {
+					ImageOptim.processIdea(self.id);
+					return self;
+				});
+      },
+      
+    }
 	});
 	
 	function scopes() {
@@ -76,8 +124,28 @@ module.exports = function( db, sequelize, DataTypes ) {
 			},
 			asTile: {
 				attributes: ['id', 'image', 'title', 'summary'],
-				order: 'createdAt DESC'
-			}
+				include: [{
+					model      : db.Image,
+					as         : 'posterImage',
+					attributes : ['key'],
+					required   : false,
+					where      : {
+						sort: 0
+					}
+				}],
+				order: 'article.createdAt DESC'
+			},
+			withPosterImage: {
+				include: [{
+					model      : db.Image,
+					as         : 'posterImage',
+					attributes : ['key'],
+					required   : false,
+					where      : {
+						sort: 0
+					}
+				}]
+			},
 		};
 	}
 	
