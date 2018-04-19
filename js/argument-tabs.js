8b86bdce98577b4f78935a2aa84ae67e429964e7
Tosh Koevoets <tosh.koevoets@gmail.com>
2018-04-19 22:34:26 +0200

    Filter select for mobile

--- js/argument-tabs.js
+++ js/argument-tabs.js
@@ -1,33 +1,43 @@
 jQuery( document ).ready(function( $ ) {
   var $tabs = $('.argument-filter-tab');
 
+  $('#filter-arguments-select').on('change', function() {
+    filterVariant($(this).val());
+  });
+
   $tabs.on('click', function(event) {
     event.preventDefault();
     var $thisTab = $(this);
     var variant = $thisTab.attr('data-variant');
-
-    if(!$thisTab.hasClass('active') ) {
-      $tabs.removeClass('active');
-      $thisTab.addClass("active");
-
-      //hide all argumtens and no
-      $('.argument, .reaction, .no-arguments').hide();
-
-      var filterSelector = variant === 'all' ? '.argument, .reaction' : '.argument-variant-' + variant;
-      var $argumentsToDispay = $(filterSelector);
-      var forArgumentsCount = $('.for ' + filterSelector).length;
-      var againstArgumentsCount = $('.against ' + filterSelector).length;
-
-      if (forArgumentsCount === 0) {
-        $('.for .no-arguments').show();
-      }
-
-      if (againstArgumentsCount === 0) {
-        $('.against .no-arguments').show();
-      }
-
-      $argumentsToDispay.show();
-    }
+    filterVariant(variant);
   });
+
+
+  function filterVariant(variant) {
+    var $thisTab = $('[data-variant="'+variant+'"]');
+    var $select = $('#filter-arguments-select');
+
+    $tabs.removeClass('active');
+    $thisTab.addClass('active');
+    $select.val(variant);
+
+    //hide all argumtens and no
+    $('.argument, .reaction, .no-arguments').hide();
+
+    var filterSelector = variant === 'all' ? '.argument, .reaction' : '.argument-variant-' + variant;
+    var $argumentsToDispay = $(filterSelector);
+    var forArgumentsCount = $('.for ' + filterSelector).length;
+    var againstArgumentsCount = $('.against ' + filterSelector).length;
+
+    if (forArgumentsCount === 0) {
+      $('.for .no-arguments').show();
+    }
+
+    if (againstArgumentsCount === 0) {
+      $('.against .no-arguments').show();
+    }
+
+    $argumentsToDispay.show();
+  }
 
 });
