3c2ebcfffe73f157440e64487b28aa78ffae3613
Tosh Koevoets <tosh.koevoets@gmail.com>
2018-04-25 21:50:57 +0200

    Fix slow arguments query

--- src/routes/stemtool/default.js
+++ src/routes/stemtool/default.js
@@ -9,44 +9,69 @@ var auth         = require('../../auth');
 module.exports = function( app ) {
 
 	app.route('/disclaimer')
-	.get(function(req, res) {
-		res.out('disclaimer', false);
-	});
+	  .get(function(req, res) {
+		  res.out('disclaimer', false);
+	  });
 
 	// View idea
 	// ---------
 	app.route('/')
-	.all(function( req, res, next ) {
-		db.Idea.scope(
-			'summary',
-			'withUser',
-			'withVoteCount',
-			'withPosterImage',
-			{method: ['withArguments', req.user.id]},
-			'withAgenda'
-		)
-		.findById(1)
-		.then(function( idea ) {
-			if( !idea ) {
-				next(createError(404, 'Plan niet gevonden'));
-			} else {
-				req.idea = idea;
-				next();
-			}
-		})
-		.catch(next);
-	})
-	.all(fetchPoll)
-	.all(auth.can('idea:admin', 'poll:vote', 'arg:add', true))
-	.get(function( req, res, next) {
-		res.out('index', true, {
-			user      : req.user,
-			idea      : req.idea,
-			poll      : req.poll,
-			userVote  : req.vote,
-			csrfToken : req.csrfToken()
-		});
-	});
+	  .all(function( req, res, next ) {
+		  db.Idea.scope(
+			  'summary',
+			  'withUser',
+			  'withVoteCount',
+			  'withPosterImage',
+			  'withAgenda'
+		  )
+		    .findById(1)
+		    .then(function( idea ) {
+			    if( !idea ) {
+				    next(createError(404, 'Plan niet gevonden'));
+			    } else {
+				    req.idea = idea;
+			    }
+		    })
+		    .then(function() {
+          return db.Argument.scope(
+					  'defaultScope',
+					  {method: ['withVoteCount', 'argument']},
+					  {method: ['withUserVote', 'argument', req.user.id]},
+					  'withReactions'
+			    )
+            .findAll({where: {ideaId: 1, sentiment: 'against'}})
+            .then(arguments1 => {
+              req.idea.argumentsAgainst = arguments1;
+            })
+        })
+		    .then(function() {
+          return db.Argument.scope(
+						'defaultScope',
+						{method: ['withVoteCount', 'argument']},
+						{method: ['withUserVote', 'argument', req.user.id]},
+						'withReactions'
+			    )
+          .findAll({where: {ideaId: 1, sentiment: 'for'}})
+          .then((arguments2) => {
+            req.idea.argumentsFor = arguments2;
+          })
+        })
+		    .then(function() {
+				  next();
+        })
+		    .catch(next);
+	  })
+	  .all(fetchPoll)
+	  .all(auth.can('idea:admin', 'poll:vote', 'arg:add', true))
+	  .get(function( req, res, next) {
+		  res.out('index', true, {
+			  user      : req.user,
+			  idea      : req.idea,
+			  poll      : req.poll,
+			  userVote  : req.vote,
+			  csrfToken : req.csrfToken()
+		  });
+	  });
 
 
 
@@ -70,13 +95,13 @@ function fetchPoll( req, res, next ) {
 			ideaId: ideaId
 		}
 	})
-	.then(function( poll ) {
-		if( !poll ) {
-			throw createError(404, 'Poll niet gevonden');
-		}
+	  .then(function( poll ) {
+		  if( !poll ) {
+			  throw createError(404, 'Poll niet gevonden');
+		  }
 
-		req.poll = poll;
-		next();
-	})
-	.catch(next);
+		  req.poll = poll;
+		  next();
+	  })
+	  .catch(next);
 }
