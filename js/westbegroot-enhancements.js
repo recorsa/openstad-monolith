1cb19fde83f05b1d7330ee31f9be7ef1f57979ba
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-01-03 18:02:48 +0100

    Don't add shadow on hover targe

--- js/westbegroot-enhancements.js
+++ js/westbegroot-enhancements.js
@@ -15,10 +15,11 @@ function initImagesOnHover () {
   $('#budget-block').on('mouseover', '.idea-image-mask', function () {
     $('#budget-block .idea-image-mask').addClass('greyed-out');
     $('#budget-block .idea-' + $(this).attr('data-idea-id')).removeClass('greyed-out').addClass('active');
+    $('#budget-block .idea-' + $(this).attr('data-idea-id')).not(this).addClass('not-target');
   });
 
   $('#budget-block').on('mouseout', '.idea-image-mask', function () {
-    $('#budget-block .idea-' + $(this).attr('data-idea-id')).removeClass('active');
+    $('#budget-block .idea-' + $(this).attr('data-idea-id')).removeClass('active not-target');
     $('#budget-block .idea-image-mask.greyed-out').removeClass('greyed-out');
   });
 
