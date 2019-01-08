8bc7d4dddf5425b6034b41e16b907a5e10095ce1
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-01-08 15:33:07 +0100

    styling fixes

--- js/ideas-gridder-list.js
+++ js/ideas-gridder-list.js
@@ -13,16 +13,20 @@ $(function() {
     prevText: "<span></span>", // Previous button text
     closeText: "", // Close button text                // Close button text
     onStart: function(target) {
+
+			console.log('fsdsdfsdfsfd');
+			var stickyHeight = $(window).width() > 767 ? 76 : 109;
+
 			var isPhone = document.querySelector('body').offsetWidth < 700; // isPhone - todo: betere afvanging
-			this.scrollOffset = isPhone ? -40 : 100;
+			this.scrollOffset = stickyHeight - 12;// isPhone ? -40 : 100;
 		},
     onContent: function(args){
 
 			var element = args[0];
 			var ideaId = element.querySelector('.this-idea-id').innerHTML;
-			 
+
 			window.history.replaceState({}, '', '#showidea-' + ideaId);
-			 
+
 			return false;
 
 		},
