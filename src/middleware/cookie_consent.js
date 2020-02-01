238d57a9e326609f3e59e785239cae7a3a422f59
Niels Vegter <niels@denes.nl>
2020-02-01 13:31:48 +0000

    Add cookie consent

--- src/middleware/cookie_consent.js
+++ src/middleware/cookie_consent.js
@@ -1,21 +1,8 @@
 var config = require('config');
 
 module.exports = function( req, res, next ) {
-	var consent = (
-	              	req.cookies &&
-	              	req.cookies.cookieConsent == 'true'
-	              );
-	req.cookieConsent = consent;
-	res.locals.cookieConsent = consent;
-	
-	res.acceptCookies = function() {
-		if( consent ) return;
-		res.cookie('cookieConsent', 'true', {
-			maxAge   : 2 * 31536000000, // 2 years
-			httpOnly : true,
-			secure   : !config.get('debug')
-		});
-	};
-	
+
+	res.locals.cookieConsent = req.cookies && req.cookies.cookieConsent;
 	next();
+
 };
--- src/middleware/nunjucks.js
+++ src/middleware/nunjucks.js
@@ -4,9 +4,9 @@
 
 module.exports = function( app ) {
 	app.use(function( req, res, next ) {
-		var fullHost        = req.protocol + '://' + req.hostname;
-		res.locals.hostname = req.hostname;
-		res.locals.url      = fullHost + req.originalUrl;
+		var fullHost             = req.protocol + '://' + req.hostname;
+		res.locals.hostname      = req.hostname;
+		res.locals.url           = fullHost + req.originalUrl;
 		
 		next();
 	});
